1d0b6b4917e300d6db07ed6d4e80d363f36a616e
lansmurf04 <lansmurf04@gmail.com>
2023-08-27 23:23:23 -0500

    feature: destroy productos

--- src/app/productos/productos-index/productos-index.component.ts
+++ src/app/productos/productos-index/productos-index.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Producto } from 'src/app/models/producto.entity';
-import { ProductosService } from 'src/app/services/productos.service';
+import { ProductoService } from 'src/app/services/producto.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-productos-index',
@@ -10,17 +11,55 @@ import { ProductosService } from 'src/app/services/productos.service';
 export class ProductosIndexComponent implements OnInit {
   productos: Producto[] = [];
 
-  constructor(private productosService: ProductosService) {}
+  constructor(private productoService: ProductoService) {}
 
   ngOnInit(): void {
     this.cargarProductos();
   }
 
   cargarProductos() {
-    this.productosService.getProductos().subscribe({
+    this.productoService.getProductos().subscribe({
       next: (data) => {
         this.productos = data;
       },
+    });
+  }
+
+  borrarProducto(producto: Producto) {
+    Swal.fire({
+      title: 'Â¿Seguro quieres borrar?',
+      html: `
+      <p>Estarias borrando el producto "${producto.nombre}"</p>
+    `,
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Si',
+      cancelButtonText: 'No',
+      // preConfirm: () => {
+      //   const selectElement = document.getElementById(
+      //     "selectElement"
+      //   ) as HTMLSelectElement;
+      //   const selectedValue = selectElement.value;
+      //   if (!selectedValue) {
+      //     Swal.showValidationMessage("Por favor selecciona un valor");
+      //     return false;
+      //   }
+      //   return selectedValue;
+      // },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.productoService.destroyProducto(producto.id).subscribe({
+          next: () => {
+            Swal.fire({
+              title: 'Producto',
+              text: 'El producto fue borrado',
+              icon: 'success',
+            });
+            // Fetch the updated list of stations here
+            this.cargarProductos();
+          },
+        });
+      }
     });
   }
 }
