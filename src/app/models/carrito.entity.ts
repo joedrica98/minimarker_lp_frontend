e0a5b474b6cc0d92cbf4da19d645593e46023d51
lansmurf04 <lansmurf04@gmail.com>
2023-09-05 21:54:17 -0500

    feature: crear modelo carrito, item carrito

---
+++ src/app/models/carrito.entity.ts
@@ -0,0 +1,6 @@
+import { Item } from './item.entity';
+
+export class Carrito {
+  items: Item[];
+  total: number;
+}
---
+++ src/app/models/item.entity.ts
@@ -0,0 +1,7 @@
+export class Item {
+  nombre: string;
+  descripcion: string;
+  precio: string;
+  cantidad: number;
+  subtotal: number;
+}
